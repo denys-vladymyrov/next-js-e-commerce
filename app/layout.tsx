bbe2fd3432281f888210eb6d5ed70bd3c6658720
Denys Vladymyrov <link_amd@yahoo.com>
2025-02-15 23:48:22 +0200

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import '@/assets/styles/globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+    title: 'Next Js E-Commerce',
+    description: 'A modern store built with Next.js',
+};
+
+export default function RootLayout({
+   children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang='en' suppressHydrationWarning>
+        <body className={`${inter.className}`}>{children}</body>
+        </html>
+    );
+}
