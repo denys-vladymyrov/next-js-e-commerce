a02384e0f9062dfc01a48f888dfa264d3a85ab5f
Denys Vladymyrov <link_amd@yahoo.com>
2025-03-01 19:05:00 +0200

    added shadcnUi and constants

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '@/assets/styles/globals.css';
+import {APP_DESCRIPTION, APP_NAME} from "@/lib/constants";
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-    title: 'Next Js E-Commerce',
-    description: 'A modern store built with Next.js',
+    title: APP_NAME,
+    description: APP_DESCRIPTION,
 };
 
 export default function RootLayout({
