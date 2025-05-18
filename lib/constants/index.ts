d180d676f5ac59848665cce2b938ae60244b56f7
Denys Vladymyrov <link_amd@yahoo.com>
2025-05-18 21:47:35 +0300

    added sign up page and form

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -7,3 +7,11 @@ export const signInDefaultValues = {
     email: 'admin@example.com',
     password: '123456',
 };
+
+export const signUpDefaultValues = {
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+};
+
