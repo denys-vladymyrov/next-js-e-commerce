c7da0102759d409d56ebc4382a5fbeb80ee3d030
Denys Vladymyrov <link_amd@yahoo.com>
2025-06-04 02:56:52 +0300

    added "cart page"

--- components/shared/product/product-price.tsx
+++ components/shared/product/product-price.tsx
@@ -8,9 +8,15 @@ const ProductPrice = ({
     className?: string;
 }) => {
     // Ensure two decimal places
+
+    console.log('value',  value);
+
     const stringValue = value.toFixed(2);
     // Get the int/float
     const [intValue, floatValue] = stringValue.split('.');
+
+    console.log(intValue);
+    console.log(floatValue);
 
     return (
         <p className={cn('text-2xl', className)}>
