e3e824cdde136245cf06b2bcb42a4d9c898f8cf8
Denys Vladymyrov <link_amd@yahoo.com>
2025-06-04 02:18:36 +0300

    added "removeItemFromCart" action

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -4,7 +4,8 @@ import { Card, CardContent } from '@/components/ui/card';
 import { getProductBySlug } from '@/lib/actions/product.actions';
 import { Badge } from '@/components/ui/badge';
 import ProductImages from '@/components/shared/product/product-images';
-import AddToCart from "@/components/shared/product/add-to-cart";
+import AddToCart from '@/components/shared/product/add-to-cart';
+import { getMyCart } from '@/lib/actions/cart.actions';
 
 const ProductDetailsPage = async (props: {
     params: Promise<{ slug: string }>;
@@ -15,6 +16,8 @@ const ProductDetailsPage = async (props: {
 
     const product = await getProductBySlug(slug);
     if (!product) notFound();
+
+    const cart = await getMyCart();
 
     return (
         <>
@@ -69,14 +72,17 @@ const ProductDetailsPage = async (props: {
                                 </div>
                                 {product.stock > 0 && (
                                     <div className=' flex-center'>
-                                        <AddToCart item={{
-                                            productId: product.id,
-                                            name: product.name,
-                                            slug: product.slug,
-                                            price: product.price,
-                                            qty: 1,
-                                            image: product.images![0]
-                                        }} />
+                                        <AddToCart
+                                            cart={cart}
+                                            item={{
+                                                productId: product.id,
+                                                name: product.name,
+                                                slug: product.slug,
+                                                price: product.price,
+                                                qty: 1,
+                                                image: product.images![0]
+                                            }}
+                                        />
                                     </div>
                                 )}
                             </CardContent>
