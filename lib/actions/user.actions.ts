84ed02d99bd7758ba8db05c3681742cba07bd192
Denys Vladymyrov <link_amd@yahoo.com>
2025-06-09 00:40:40 +0300

    added "shipping-address" page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,10 +1,11 @@
 'use server';
 
-import { signIn, signOut } from '@/auth';
-import {signInFormSchema, signUpFormSchema} from '../validators';
+import {auth, signIn, signOut} from '@/auth';
+import {shippingAddressSchema, signInFormSchema, signUpFormSchema} from '../validators';
 import {prisma} from "@/db/prisma";
 import { hashSync } from "bcrypt-ts-edge";
 import { formatError } from "@/lib/utils";
+import {ShippingAddress} from "@/types";
 
 export async function signInWithCredentials(
     prevState: unknown,
@@ -82,6 +83,42 @@ export async function signUpUser(prevState: unknown, formData: FormData) {
             };
         }
 
+        return { success: false, message: formatError(error) };
+    }
+}
+
+// Get user by the ID
+export async function getUserById(userId: string) {
+    const user = await prisma.user.findFirst({
+        where: { id: userId },
+    });
+    if (!user) throw new Error('User not found');
+    return user;
+}
+
+// Update user's address
+export async function updateUserAddress(data: ShippingAddress) {
+    try {
+        const session = await auth();
+
+        const currentUser = await prisma.user.findFirst({
+            where: { id: session?.user?.id },
+        });
+
+        if (!currentUser) throw new Error('User not found');
+
+        const address = shippingAddressSchema.parse(data);
+
+        await prisma.user.update({
+            where: { id: currentUser.id },
+            data: { address },
+        });
+
+        return {
+            success: true,
+            message: 'User updated successfully',
+        };
+    } catch (error) {
         return { success: false, message: formatError(error) };
     }
 }
