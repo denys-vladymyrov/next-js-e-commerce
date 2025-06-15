b83a81d96eb5273d56201b063fb6344995afd4fd
Denys Vladymyrov <link_amd@yahoo.com>
2025-06-15 23:19:04 +0300

    added 'insertOrderSchema'

--- types/index.ts
+++ types/index.ts
@@ -4,11 +4,28 @@ import {
     insertCartSchema,
     cartItemSchema,
     shippingAddressSchema,
+    insertOrderSchema,
+    insertOrderItemSchema,
+    paymentResultSchema,
 } from '@/lib/validators';
 
 export type Cart = z.infer<typeof insertCartSchema>;
 export type CartItem = z.infer<typeof cartItemSchema>;
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
+export type OrderItem = z.infer<typeof insertOrderItemSchema>;
+export type PaymentResult = z.infer<typeof paymentResultSchema>;
+
+export type Order = z.infer<typeof insertOrderSchema> & {
+    id: string;
+    createdAt: Date;
+    isPaid: boolean;
+    paidAt: Date | null;
+    isDelivered: boolean;
+    deliveredAt: Date | null;
+    orderitems: OrderItem[];
+    user: { name: string; email: string };
+    paymentResult: PaymentResult;
+};
 
 export type Product = z.infer<typeof insertProductSchema> & {
     id: string;
